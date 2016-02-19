60558a256376d69ad4873aa9239773e774453221
Randy Houlahan <randy.houlahan@cbd.int>
2016-02-19 18:25:48 -0500

    updates for build

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker build -t localhost:5000/plevra-cbd-int git@github.com:scbd/plevra.cbd.int.git
-docker push     localhost:5000/plevra-cbd-int
+docker build -t localhost:5000/inde-cbd-int git@github.com:scbd/inde.cbd.int.git
+docker push     localhost:5000/inde-cbd-int
