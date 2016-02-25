a29dc493666039594a3d700c4c19b07282299a9e
Randy Houlahan <randy.houlahan@cbd.int>
2016-02-24 23:51:30 -0500

    style updates to org form

--- app/views/manage/organizations.js
+++ app/views/manage/organizations.js
@@ -1,34 +1,54 @@
 define(['app', 'lodash',
   'css!./organizations',
   'scbd-branding/side-menu/scbd-side-menu',
+  'scbd-branding/scbd-button',
   'scbd-branding/side-menu/scbd-menu-service',
   'scbd-angularjs-controls/km-inputtext-ml',
   'scbd-angularjs-controls/km-control-group',
-
+'scbd-branding/scbd-icon-button',
+'scbd-branding/scbd-tooltip',
   'scbd-angularjs-controls/km-select',
     'scbd-angularjs-controls/km-form-languages',
     'scbd-angularjs-controls/km-inputtext-list',
-    'scbd-angularjs-services/locale',
+
+    '../../services/mongo-storage'
 
 ], function(app, _) { //'scbd-services/utilities',
 
   // If you specify less than all of the keys, it will inherit from the
   // default shades
 
-  app.controller("organizations", ['$scope', 'scbdMenuService', '$q', '$http','locale','$filter', //"$http", "$filter", "Thesaurus",
-    function($scope, scbdMenuService, $q, $http,locale,$filter) { //, $http, $filter, Thesaurus
+  app.controller("organizations", ['$scope', 'scbdMenuService', '$q', '$http','$filter','$route','mongoStorage','$location', //"$http", "$filter", "Thesaurus",
+    function($scope, scbdMenuService, $q, $http,$filter,$route,mongoStorage,$location) { //, $http, $filter, Thesaurus
 
-      $scope.locales = [locale];
+
+      $scope._id = $route.current.params.id || false;
+      $scope.loading=false;
+
       $scope.toggle = scbdMenuService.toggle;
       $scope.dashboard = scbdMenuService.dashboard;
-      console.log('scbdMenuService', locale);
+
+      if($scope._id ){
+          if($scope._id.search('^[0-9A-Fa-f]{24}$')<0)
+            $location.url('/404');
+          else
+            mongoStorage.loadDoc('inde-orgs',$scope._id).then(function(document){
+                  $scope.loading=true;
+                  $scope._id=document[0];
+                  $scope.doc=document[1];
+                  console.log('$scope._id',$scope._id);
+                  console.log('$scope.doc',$scope.doc);
+            });
+        }
+      else
+        $scope.doc=mongoStorage.createDoc();
+
 
       $scope.options = {
           countries: function() {
               return $http.get("https://api.cbd.int/api/v2015/countries", {
                   cache: true
               }).then(function(o) {
-                console.log('000000',o);
                   return $filter("orderBy")(o.data, "name");
               });
           },
@@ -41,10 +61,33 @@ define(['app', 'lodash',
           }
       };
 
-            $q.when( $http.get('/api/v2015/inde-orgs'))
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.saveDoc = function(){
+
+        //delete($scope.doc.meta);
+
+          mongoStorage.save('inde-orgs',$scope.doc,$scope._id).then(function(data){
+                if(!$scope._id){ $scope._id=data.id;
+                        $location.path($location.path()+'/'+$scope._id,false);
+                }
+          });
+      };
+
+
+
+      ///api/v2015/users?q={ "lastName" : "smith" }&f={ "email" : 1,"firstName" : 1,"lastName" : 1}
+//&f={ "email" : 1,"firstName" : 1,"lastName" : 1}
+//?q={ "lastName" : "smith" }&fo=1
+//q={"_id":{"$oid":"56c52697c68ede024eef5da4"}&f={"document":1}
+
+//q={"_id":{"$oid":"52000000cbd0316600000014"}&f={"treaties":1}
+
+            $q.when( $http.get('/api/v2015/inde-orgs?q={"document.v": 3,"_id":{"$oid":"56c52697c68ede024eef5da4"}}&f={"document":1}'))
            .then(function(response){
-                $scope.faqSearch = response.data;
-      console.log(response.data);
+              //  $scope.faqSearch = response.data;
+      ///console.log(response.data.length);
               });
       //=======================================================================
         //
