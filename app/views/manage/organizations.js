627bd606c3472ccf7c0d184735b078f482becb7c
Randy Houlahan <randy.houlahan@cbd.int>
2016-02-29 10:13:30 -0500

    organization view

--- app/views/manage/organizations.js
+++ app/views/manage/organizations.js
@@ -3,14 +3,9 @@ define(['app', 'lodash',
   'scbd-branding/side-menu/scbd-side-menu',
   'scbd-branding/scbd-button',
   'scbd-branding/side-menu/scbd-menu-service',
-  'scbd-angularjs-controls/km-inputtext-ml',
-  'scbd-angularjs-controls/km-control-group',
-'scbd-branding/scbd-icon-button',
-'scbd-branding/scbd-tooltip',
-  'scbd-angularjs-controls/km-select',
-    'scbd-angularjs-controls/km-form-languages',
-    'scbd-angularjs-controls/km-inputtext-list',
-
+  '../../directives/scbd-localizer',
+    'scbd-branding/scbd-icon-button',
+    'scbd-branding/scbd-tooltip',
     '../../services/mongo-storage'
 
 ], function(app, _) { //'scbd-services/utilities',
@@ -22,60 +17,124 @@ define(['app', 'lodash',
     function($scope, scbdMenuService, $q, $http,$filter,$route,mongoStorage,$location) { //, $http, $filter, Thesaurus
 
 
-      $scope._id = $route.current.params.id || false;
+
       $scope.loading=false;
+      $scope.schema="inde-orgs";
 
       $scope.toggle = scbdMenuService.toggle;
       $scope.dashboard = scbdMenuService.dashboard;
 
-      if($scope._id ){
-          if($scope._id.search('^[0-9A-Fa-f]{24}$')<0)
-            $location.url('/404');
-          else
-            mongoStorage.loadDoc('inde-orgs',$scope._id).then(function(document){
-                  $scope.loading=true;
-                  $scope._id=document[0];
-                  $scope.doc=document[1];
-                  console.log('$scope._id',$scope._id);
-                  console.log('$scope.doc',$scope.doc);
-            });
-        }
-      else
-        $scope.doc=mongoStorage.createDoc();
+      $scope.orgs;
 
+      $scope.sortReverse=0;
+      $scope.listView=0;//list,tiles,details
+      $scope.showArchived=0;
 
-      $scope.options = {
-          countries: function() {
-              return $http.get("https://api.cbd.int/api/v2015/countries", {
-                  cache: true
-              }).then(function(o) {
-                  return $filter("orderBy")(o.data, "name");
-              });
-          },
-          organizationTypes: function() {
-              return $http.get("https://api.cbd.int/api/v2013/thesaurus/domains/Organization%20Types/terms", {
-                  cache: true
-              }).then(function(o) {
-                  return o.data;
-              });
-          }
-      };
+      // $scope.options = {
+      //     countries: function() {
+      //         return $http.get("https://api.cbd.int/api/v2015/countries", {
+      //             cache: true
+      //         }).then(function(o) {
+      //             return $filter("orderBy")(o.data, "name");
+      //         });
+      //     },
+      //     organizationTypes: function() {
+      //         return $http.get("https://api.cbd.int/api/v2013/thesaurus/domains/Organization%20Types/terms", {
+      //             cache: true
+      //         }).then(function(o) {
+      //             return o.data;
+      //         });
+      //     }
+      // };
 
       //=======================================================================
       //
       //=======================================================================
-      $scope.saveDoc = function(){
+      function init(){
 
-        //delete($scope.doc.meta);
+//               $q.when( $http.get('/api/v2015/inde-orgs?f={"document":1}'))
+//              .then(function(response){
+// console.log(response.data);
+//                 $scope.orgs=response.data;
+//
+//               });
+              $scope.loadList ();
+      }//init
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.archiveList = function (){
+        mongoStorage.loadArchives($scope.schema).then(function(response){
+          $scope.orgs=response.data;
+        });
+      };// archiveOrg
 
-          mongoStorage.save('inde-orgs',$scope.doc,$scope._id).then(function(data){
-                if(!$scope._id){ $scope._id=data.id;
-                        $location.path($location.path()+'/'+$scope._id,false);
-                }
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.loadList = function (docObj){
+        mongoStorage.loadDocs($scope.schema).then(function(response){
+           $scope.orgs=response.data;
+         });
+      };// archiveOrg
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.toggleListView = function (docObj){
+
+        if($scope.listView===0)
+            $scope.listView=1;
+        else if($scope.listView===1)
+            $scope.listView=2;
+        else
+          $scope.listView=0;
+      };//toggleListView
+
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.toggleArchived = function (docObj){
+console.log($scope.showArchived);
+        if(!$scope.showArchived)
+            $scope.archiveList();
+        else
+            $scope.loadList();
+        $scope.showArchived=!$scope.showArchived;
+
+      };// archiveOrg
+
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.archiveOrg = function (docObj){
+          mongoStorage.archiveDoc('inde-orgs',docObj,docObj._id).then(function(){
+                _.remove($scope.orgs,function(obj){return obj._id===docObj._id;});
           });
-      };
 
-
+      };// archiveOrg
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.unArchiveOrg = function (docObj){
+        console.log();
+        docObj.document.meta.status="draft";
+          mongoStorage.unArchiveDoc('inde-orgs',docObj,docObj._id).then(function(){
+                _.remove($scope.orgs,function(obj){return obj._id===docObj._id;});
+          });
+      };// archiveOrg
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.goTo = function (url){
+        $location.url(url);
+      }// archiveOrg
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.edit = function (id){
+        $location.url('/manage/organizations/'+id);
+      }// archiveOrg
+init();
 
       ///api/v2015/users?q={ "lastName" : "smith" }&f={ "email" : 1,"firstName" : 1,"lastName" : 1}
 //&f={ "email" : 1,"firstName" : 1,"lastName" : 1}
@@ -84,11 +143,11 @@ define(['app', 'lodash',
 
 //q={"_id":{"$oid":"52000000cbd0316600000014"}&f={"treaties":1}
 
-            $q.when( $http.get('/api/v2015/inde-orgs?q={"document.v": 3,"_id":{"$oid":"56c52697c68ede024eef5da4"}}&f={"document":1}'))
-           .then(function(response){
-              //  $scope.faqSearch = response.data;
-      ///console.log(response.data.length);
-              });
+      //       $q.when( $http.get('/api/v2015/inde-orgs?q={"document.v": 3,"_id":{"$oid":"56c52697c68ede024eef5da4"}}&f={"document":1}'))
+      //      .then(function(response){
+      //         //  $scope.faqSearch = response.data;
+      // ///console.log(response.data.length);
+      //         });
       //=======================================================================
         //
         //=======================================================================
@@ -111,9 +170,7 @@ define(['app', 'lodash',
         //   console.log($scope.countries);
         // });
 
-      $scope.organization = {};
-      $scope.organization.facebook = "ddd";
-      $scope.organization.title = "ddd";
+
       // $scope.$watch('organization',function(){console.log($scope.organizationForm.title.$error);},true);
 
     }
