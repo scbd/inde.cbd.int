2f483af60c5c980dfb2d8a10edc410fb93cdac58
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-04 06:39:52 -0500

    filter to return names of schema

--- app/services/filters.js
+++ app/services/filters.js
@@ -11,13 +11,28 @@ app.filter('schemaName', [function() {
 
           if(!schema)
             return schema;
-          if(schema.toLowerCase()=='inde-orgs') return 'Organization';
+          if(schema.toLowerCase()=='inde-orgs') return 'Organizations';
           if(schema.toLowerCase()=='inde-side-events') return 'Side Events';
           if(schema.toLowerCase()=='confrences') return 'Confrences';
-          if(schema.toLowerCase()=='venue') return 'Venue';
+          if(schema.toLowerCase()=='venue') return 'Venues';
         };
   }]);
+  //============================================================
+  //
+  //============================================================
+  app.filter('schemaNameSingular', [function() {
 
+    return function( schema ) {
+      //return mapSchema(schema);
+
+            if(!schema)
+              return schema;
+            if(schema.toLowerCase()=='inde-orgs') return 'Organization';
+            if(schema.toLowerCase()=='inde-side-events') return 'Side Event';
+            if(schema.toLowerCase()=='confrences') return 'Confrence';
+            if(schema.toLowerCase()=='venue') return 'Venue';
+          };
+    }]);
   //============================================================
   //
   //============================================================
@@ -33,7 +48,7 @@ app.filter('schemaName', [function() {
             if(schema.toLowerCase()=='venue') return 'md';
           };
     }]);
-    
+
     //============================================================
     //
     //============================================================
