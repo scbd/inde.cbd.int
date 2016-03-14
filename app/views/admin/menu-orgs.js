cc7c559cd3833d4a3c8fa095b4bafc7e42fe5df0
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-14 07:57:56 -0400

    add separate menu for orgs

---
+++ app/views/admin/menu-orgs.js
@@ -0,0 +1,171 @@
+define(['app','../../directives/side-menu/scbd-side-menu'], function (app,_) {
+
+app.factory("adminOrgMenu", ['scbdMenuService', function(scbdMenuService) {
+
+
+        //var extended = angular.extend(scbdMenuService, {});
+        //console.log(extended);
+        scbdMenuService.menus.admin= [];
+        scbdMenuService.menus.admin.push({
+          type: 'config',
+          menuClass:'dash-menu',
+          colorClass: 'dash-menu-color',
+          activeClass: 'dash-menu-active',
+          iconClass: 'pulse',
+          selfMenu: scbdMenuService.menus.admin,// needed shouls be added programatically in parent service
+          childrenColorClass: 'dash-menu-children-color',
+          childrenActiveClass: 'dash-menu-children-active'
+        });
+        scbdMenuService.menus.admin.push({
+          name: 'Admin Dashboard',
+          type: 'link',
+          mdIcon: 'dashboard',
+          path: '/admin',
+          roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.admin.push({
+          name: 'Side Events',
+          type: 'link',
+          path: '/admin/events',
+          mdIcon: 'event',
+          roles:['Administrator','IndeAdministrator'],
+        });
+
+        scbdMenuService.menus.admin.push({
+          name: 'Organizations',
+          type: 'link',
+          path: '/admin/organizations',
+          mdIcon: 'business',
+          roles:['Administrator','IndeAdministrator'],
+        });
+
+        scbdMenuService.menus.adminOrgOptions= [];
+        scbdMenuService.menus.adminOrgOptions.push({
+          type: 'config',
+          menuClass:'dash-menu',
+          colorClass: 'dash-menu-color',
+          activeClass: 'dash-menu-active',
+          iconClass: 'pulse',
+          selfMenu: scbdMenuService.menus.adminOrgOptions,// needed shouls be added programatically in parent service
+          childrenColorClass: 'dash-menu-children-color',
+          childrenActiveClass: 'dash-menu-children-active'
+        });
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'New',
+          type: 'link',
+          mdIcon: 'add_box',
+          path: '/manage/organizations/new',
+          roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'Unarchived',
+          type: 'link',
+          mdIcon: 'archive',
+          path: '/admin/organizations/',
+                  roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'Archives',
+          type: 'link',
+          mdIcon: 'archive',
+          path: '',
+          roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'Sort',
+          type: 'link',
+          mdIcon: 'sort_by_alpha',
+          path: '',
+          roles:['Administrator','IndeAdministrator'],
+        });
+
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'Filter',
+          type: 'toggle',
+          open:0,
+          mdIcon: 'filter_list',
+          roles:['Administrator','IndeAdministrator'],
+          pages: [
+            {
+              name: 'All',
+              type: 'link',
+              path: '',
+              mdIcon: 'all_inclusive',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'Drafts',
+              type: 'link',
+              path: '',
+              mdIcon: 'insert_drive_file',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'Requests',
+              type: 'link',
+              path: '',
+              mdIcon: 'live_help',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'Approved',
+              type: 'link',
+              path: '',
+              mdIcon: 'thumb_up',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'Canceled',
+              type: 'link',
+              path: '',
+              mdIcon: 'thumbs_up_down',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'Rejected',
+              type: 'link',
+              path: '',
+              mdIcon: 'view_module',
+              roles:['Administrator','IndeAdministrator'],
+            },
+
+          ]
+        });
+        scbdMenuService.menus.adminOrgOptions.push({
+          name: 'View',
+          type: 'toggle',
+          open:0,
+          mdIcon: 'remove_red_eye',
+          roles:['Administrator','IndeAdministrator'],
+          pages: [
+            {
+              name: 'Card View',
+              type: 'link',
+              path: '',
+              mdIcon: 'view_module',
+              roles:['Administrator','IndeAdministrator'],
+            },
+            {
+              name: 'List View',
+              type: 'link',
+              path: '',
+              mdIcon: 'view_list',
+              roles:['Administrator','IndeAdministrator'],
+            },
+          //   {
+          //   name: 'Detail View',
+          //   type: 'link',
+          //   path: '/admin/events/detail-view',
+          //   mdIcon: 'view_headline',
+          //   roles:['Administrator','IndeAdministrator'],
+          //
+          // }
+        ],
+        });
+
+
+        scbdMenuService.validateMenus();// minds color classes and animation ect
+
+        return scbdMenuService;
+  }]);
+});
