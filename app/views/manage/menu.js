650ceab939e88d0b4da5566b3f3eadb7c5df708c
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-14 08:01:38 -0400

    Updates to menu to include dashboard

--- app/views/manage/menu.js
+++ app/views/manage/menu.js
@@ -1,4 +1,4 @@
-define(['app','directives/side-menu/scbd-menu-service'], function (app,_) {
+define(['app',  '../../directives/side-menu/scbd-side-menu'], function (app,_) {
 
 app.factory("dashMenu", ['scbdMenuService', function(scbdMenuService) {
 
@@ -16,25 +16,27 @@ app.factory("dashMenu", ['scbdMenuService', function(scbdMenuService) {
           childrenColorClass: 'dash-menu-children-color',
           childrenActiveClass: 'dash-menu-children-active'
         });
-        // scbdMenuService.menus.dashboard.push({
-        //   name: 'Dashboard',
-        //   type: 'link',
-        //   mdIcon: 'dashboard',
-        //   path: '/manage',
-        //
-        // });
+        scbdMenuService.menus.dashboard.push({
+          name: 'Dashboard',
+          type: 'link',
+          mdIcon: 'dashboard',
+          path: '/manage',
+              roles:['Administrator','IndeAdministrator'],
+        });
 
         scbdMenuService.menus.dashboard.push({
           name: 'Side Events',
           type: 'link',
           mdIcon: 'event',
           path: '/manage/events',
+              roles:['Administrator','IndeAdministrator'],
         });
         scbdMenuService.menus.dashboard.push({
           name: 'Organizations',
           type: 'link',
           mdIcon: 'business',
           path: '/manage/organizations',
+              roles:['Administrator','IndeAdministrator'],
         });
         scbdMenuService.validateMenus();// minds color classes and animation ect
 
@@ -62,18 +64,28 @@ app.factory("dashMenu", ['scbdMenuService', function(scbdMenuService) {
           type: 'link',
           mdIcon: 'add_box',
           path: '/manage/events/new',
+              roles:['Administrator','IndeAdministrator'],
         });
         scbdMenuService.menus.options.push({
-          name: 'View Archive',
+          name: 'Unarchived',
+          type: 'link',
+          mdIcon: 'archive',
+          path: '/manage/events/',
+              roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.options.push({
+          name: 'Archives',
           type: 'link',
           mdIcon: 'archive',
           path: '/manage/events/archived',
+              roles:['Administrator','IndeAdministrator'],
         });
         scbdMenuService.menus.options.push({
-          name: 'Sort Order',
+          name: 'Sort',
           type: 'link',
           mdIcon: 'sort_by_alpha',
           path: '/manage/events/sort',
+              roles:['Administrator','IndeAdministrator'],
         });
 
         scbdMenuService.menus.options.push({
@@ -91,7 +103,7 @@ app.factory("dashMenu", ['scbdMenuService', function(scbdMenuService) {
               roles:['Administrator','IndeAdministrator'],
             },
             {
-              name: 'Draft',
+              name: 'Drafts',
               type: 'link',
               path: '/admin/events/all',
               mdIcon: 'insert_drive_file',
@@ -155,14 +167,54 @@ app.factory("dashMenu", ['scbdMenuService', function(scbdMenuService) {
               mdIcon: 'view_list',
               roles:['Administrator','IndeAdministrator'],
             },
-            {
-            name: 'Detail View',
-            type: 'link',
-            path: '/admin/events/detail-view',
-            mdIcon: 'view_headline',
-            roles:['Administrator','IndeAdministrator'],
-
-          }],
+          //   {
+          //   name: 'Detail View',
+          //   type: 'link',
+          //   path: '/admin/events/detail-view',
+          //   mdIcon: 'view_headline',
+          //   roles:['Administrator','IndeAdministrator'],
+          //
+          // }
+        ],
+        });
+        scbdMenuService.menus.editEventOptions= [];
+        scbdMenuService.menus.editEventOptions.push({
+          type: 'config',
+          menuClass:'dash-menu',
+          colorClass: 'dash-menu-color',
+          activeClass: 'dash-menu-active',
+          iconClass: 'pulse',
+          selfMenu: scbdMenuService.menus.editEventOptions,// needed shouls be added programatically in parent service
+          childrenColorClass: 'dash-menu-children-color',
+          childrenActiveClass: 'dash-menu-children-active'
+        });
+        // scbdMenuService.menus.dashboard.push({
+        //   name: 'Dashboard',
+        //   type: 'link',
+        //   mdIcon: 'dashboard',
+        //   path: '/manage',
+        //
+        // });
+        scbdMenuService.menus.editEventOptions.push({
+          name: 'Register',
+          type: 'link',
+          mdIcon: 'add_box',
+          path: '/manage/events/new',
+              roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.editEventOptions.push({
+          name: 'Save as Draft',
+          type: 'link',
+          mdIcon: 'insert_drive_file',
+          path: '',
+              roles:['Administrator','IndeAdministrator'],
+        });
+        scbdMenuService.menus.editEventOptions.push({
+          name: 'Request Approval',
+          type: 'link',
+          mdIcon: 'live_help',
+          path: '',
+              roles:['Administrator','IndeAdministrator'],
         });
           scbdMenuService.validateMenus();// minds color classes and animation ect
         return scbdMenuService;
