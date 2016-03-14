79db55b2176e9e2697d2f95df60dc824abaa9d38
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-14 07:57:04 -0400

    updates to dashboard

--- app/views/admin/admin-dash-board.js
+++ app/views/admin/admin-dash-board.js
@@ -1,24 +1,59 @@
 define(['app', 'lodash',
-'css!./admin-dash-board',
-'scbd-branding/side-menu/scbd-side-menu',
+
   './menu',
-'scbd-branding/scbd-button',
-'scbd-branding/scbd-icon-button',
+
 'scbd-angularjs-controls',
-'../../directives/forms/controls/scbd-select-list',
-'../../directives/scbd-tip'
+  '../../services/mongo-storage'
 
 ], function(app, _) { //'scbd-services/utilities',
 
-  // If you specify less than all of the keys, it will inherit from the
-  // default shades
 
-    app.controller("adminDashBoard", ['$scope','adminMenu', //"$http", "$filter", "Thesaurus",
-     function($scope,adminMenu) { //, $http, $filter, Thesaurus
+    app.controller("adminDashBoard", ['$scope','adminMenu','authentication','$location','$timeout','mongoStorage','$window', //"$http", "$filter", "Thesaurus",
+     function($scope,dashMenu,authentication,$location,$timeout,mongoStorage,$window) { //, $http, $filter, Thesaurus
       $scope.test=[];
-      $scope.$watch('test',function(){console.log('watch in dash',$scope.test);},true);
-      $scope.toggle = adminMenu.toggle;
-      $scope.sections = adminMenu.getMenu('admin');
 
+
+      $scope.toggle = dashMenu.toggle;
+      $scope.sections = dashMenu.getMenu('admin');
+      $scope.facets={};
+      $scope.facets.all=0;
+      $scope.facets.drafts=0;
+      $scope.facets.requests=0;
+      $scope.facets.published=0;
+      $scope.facets.canceled=0;
+      $scope.facets.rejected=0;
+      $scope.facets.archived=0;
+      $scope.facetsO={};
+      $scope.facetsO.all=0;
+      $scope.facetsO.drafts=0;
+      $scope.facetsO.requests=0;
+      $scope.facetsO.published=0;
+      $scope.facetsO.canceled=0;
+      $scope.facetsO.rejected=0;
+      $scope.facetsO.archived=0;
+      var statuses=['draft','published','request','canceled','rejected','archived','deleted'];
+      mongoStorage.getStatusFacits('inde-side-events',$scope.facets,statuses);
+      mongoStorage.getStatusFacits('inde-orgs',$scope.facetsO,statuses)
+
+      authentication.getUser().then(function (user) {
+        $scope.isAuthenticated=user.isAuthenticated;
+      }).then(function(){
+        if(!$scope.isAuthenticated)
+          $('#loginDialog').modal('show');
+      });
+
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.goTo = function (url){
+        $location.url(url);
+      }// archiveOrg
+      //=======================================================================
+      //
+      //=======================================================================
+      $scope.close = function(){
+
+          $window.history.back();
+      };
     }]);
 });
