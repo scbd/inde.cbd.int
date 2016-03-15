0a30d58e8783cadb8d9fe347769cbe5807ba8af7
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-15 08:53:31 -0400

    hide footer on load

--- app/template.js
+++ app/template.js
@@ -1,55 +1,43 @@
 define(['app', 'jquery',
-'css!font-awsome-css',
-'css!app-css',
-'directives/scbd-media',
-'scbd-angularjs-services/authentication',
-'directives/portal/portal-nav',
-'scbd-branding/header/header',
-'scbd-branding/footer'], function(app, $) {
-    'use strict';
+      'css!font-awsome-css',
+      'css!app-css',
+      'directives/scbd-media',
+      'scbd-angularjs-services/authentication',
+      'directives/portal/portal-nav',
+      'scbd-branding/header/header',
+      'scbd-branding/footer'
+    ], function(app, $) {
+      'use strict';
 
-    app.controller('TemplateController', ['$scope', '$rootScope', '$window', '$location', 'authentication',  'realm','$q', function($scope, $rootScope, $window, $location, authentication,  realm,$q) {
-
-
-
-        // if ($location.protocol() == "http" && $location.host() == "chm.cbd.int")
-        //     $window.location = "https://chm.cbd.int/";
+    app.controller('TemplateController', ['$scope', '$rootScope', '$window', '$location', 'authentication',  'realm','$q','$timeout', function($scope, $rootScope, $window, $location, authentication,  realm,$q,$timeout) {
 
 
-
-
-
-
-        // $scope.test_env        = realm != 'SEP';
 
         $scope.$root.pageTitle = { text: "" };
         $rootScope.placeholderRecords=[];
-
+          $scope.routeLoaded = false;
 
         $q.when(authentication.getUser()).then(function(u){
             $scope.user = u;
         });
+        $scope.$on('$viewContentLoaded', function(event) {
+          $timeout(function(){$scope.routeLoaded = true;},1000);
 
+        });
         $scope.$on("$routeChangeSuccess", function(evt, current){
-            $scope.routeLoaded = true;
+
             $("head > title").text(current.$$route.label || "Side event Registration");
+            $scope.path = $location.path();
+        });
+        $rootScope.$on('event:auth-emailVerification', function(evt, data){
+            $scope.showEmailVerificationMessage = data.message;
         });
 
-        // $scope.$on("$routeChangeStart", function(e,r){
-        //
-        //     if(!r.$$route.progress || r.$$route.progress.start!==false)
-        //         nprogress.start();
-        // });
-        //
-        // $scope.$on("$routeChangeSuccess", function(e,r){
-        //     if(!r.$$route.progress || r.$$route.progress.stop!==false)
-        //         nprogress.done();
-        // });
 
 
         $scope.goHome               = function() { $location.path('/'); };
         $scope.currentPath          = function() { return $location.path(); };
-        // $scope.hideSubmitInfoButton = function() { return $location.path()=="/management/register"; };
+
 
         //============================================================
            //
