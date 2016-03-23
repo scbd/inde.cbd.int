8a254759545494cec820fa9565685e979d35c510
Randy Houlahan <randy.houlahan@cbd.int>
2016-03-22 22:21:03 -0400

    add thesaurus service

---
+++ app/services/theasarus.js
@@ -0,0 +1,46 @@
+define(['app','lodash','linqjs'], function (app,_,Enumerable) {
+    	console.log(Enumerable);
+  app.factory('Enumerable', [function() {
+    	console.log(Enumerable);
+  	return Enumerable;
+  }]);
+app.factory('Thesaurus', ["Enumerable", function() {
+	return {
+		buildTree : function(terms) {
+			var oTerms    = [];
+			var oTermsMap = {};
+
+			Enumerable.From(terms).ForEach(function(value) {
+				var oTerm = {
+					identifier : value.identifier,
+					name       : value.name
+				};
+
+				oTerms.push(oTerm);
+				oTermsMap[oTerm.identifier] = oTerm;
+			});
+
+			for (var i = 0; i < oTerms.length; ++i) {
+				var oRefTerm = terms [i];
+				var oBroader = oTerms[i];
+
+				if (oRefTerm.narrowerTerms && oRefTerm.narrowerTerms.length > 0) {
+					_.each(oRefTerm.narrowerTerms, function(identifier) {
+						var oNarrower = oTermsMap[identifier];
+
+						if (oNarrower) {
+							oBroader.narrowerTerms = oBroader.narrowerTerms || [];
+							oNarrower.broaderTerms = oNarrower.broaderTerms || [];
+
+							oBroader.narrowerTerms.push(oNarrower);
+							oNarrower.broaderTerms.push(oBroader);
+						}
+					}); //jshint ignore:line
+				}
+			}
+
+			return Enumerable.From(oTerms).Where("o=>!o.broaderTerms").ToArray();
+		}
+	};
+}]);
+});
