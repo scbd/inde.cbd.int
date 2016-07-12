656530ce255e0c3695dd4975a0f42ba8381590b3
Randy Houlahan <randy.houlahan@cbd.int>
2016-07-11 21:53:29 -0400

    cleaning

--- app/views/manage/edit-organization.js
+++ app/views/manage/edit-organization.js
@@ -1,26 +1,20 @@
 define(['app',
   '../../directives/forms/edit/edit-organization',
-], function(app) { 
-  app.controller("edit-organization", ['$scope',  '$window', '$route','history', //"$http", "$filter", "Thesaurus",
-    function($scope,  $window, $route,history) { //, $http, $filter, Thesaurus
+], function(app) {
+  app.controller("edit-organization", ['$scope',  '$window', '$route','history',
+    function($scope,  $window, $route,history) {
 
       $scope.loading = false;
       $scope.schema = "inde-orgs";
-
-
       $scope.isNew = true;
-      $scope._id = $route.current.params.id;
-      if ($scope._id.length > 3) $scope.isNew = false;
-
-
 
       //=======================================================================
       //
       //=======================================================================
       $scope.close = function() {
-
         history.goBack();
       };
+
       //=======================================================================
       //
       //=======================================================================
