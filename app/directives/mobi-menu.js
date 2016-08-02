0fefdd9fd06c5f254b7d26e4cd20adb0e797ae37
Randy Houlahan <randy.houlahan@cbd.int>
2016-08-02 12:44:34 -0400

    mobi menu fix big- changing states on other menu click

--- app/directives/mobi-menu.js
+++ app/directives/mobi-menu.js
@@ -1,28 +1,57 @@
 define(['app'], function(app) {
-  'use strict';
+    'use strict';
 
-  app.directive('mobiMenu',['$document' ,function($document) {
-    return {
-      restrict: 'A',
-      scope: {
-          onFileChange : "&onFile"
-      },
-      link: function($scope, $element) {
-        var  template='<span class="visible-xs" ><button class="collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse1" ><i id="mobi-menu-icon" class="fa fa-bars" style="font-size:1.66em;"></i></button></span>';
+    app.directive('mobiMenu', ['$document', '$timeout', function($document, $timeout) {
+        return {
+            restrict: 'A',
+            scope: {
+                onFileChange: "&onFile"
+            },
+            link: function($scope, $element) {
+                var template = '<span class="visible-xs" ><button class="collapsed" type="button" data-toggle="collapse" data-target=".navbar-collapse1" ><i id="mobi-menu-icon" class="fa fa-bars" style="font-size:1.66em;"></i></button></span>';
 
-          $scope.mobiMenuOpen=false;
+                $scope.mobiMenuOpen = false;
 
-          $element.prepend(angular.element(template));
-          $element.click(toggleMenu);
+                $element.prepend(angular.element(template));
+                $element.mouseup(toggleMenu);
 
-          if($document.find('.navbar-collapse1').hasClass('in')) toggleMenu();
+                var el = $element.find('#mobi-menu-icon');
+                var menuEl = $document.find('.navbar-collapse1');
 
-            function toggleMenu(){
-              var el = $element.find('#mobi-menu-icon');
-              el.toggleClass('fa-bars');
-              el.toggleClass('fa-close');
+
+                $timeout(function() {
+                    if (menuEl.hasClass('in')) {
+                        el.removeClass('fa-bars');
+                        el.addClass('fa-close');
+
+
+                    } else {
+
+                        el.removeClass('fa-close');
+                        el.addClass('fa-bars');
+
+                    }
+                });
+
+
+                function toggleMenu() {
+
+                    $timeout(function() {
+                        if (menuEl.hasClass('in')) {
+                            el.removeClass('fa-bars');
+                            el.addClass('fa-close');
+
+
+                        } else {
+
+                            el.removeClass('fa-close');
+                            el.addClass('fa-bars');
+
+                        }
+                    }, 500);
+
+                }
             }
-      }
-    }; // return
-  }]); //app.directive('searchFilterCountries
+        }; // return
+    }]); //app.directive('searchFilterCountries
 }); // define
