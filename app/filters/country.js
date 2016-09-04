5ac5d22f090b3d838493547ff10fe72784b258e4
Randy Houlahan <randy.houlahan@cbd.int>
2016-09-04 07:59:25 -0400

    filter to display country names

---
+++ app/filters/country.js
@@ -0,0 +1,32 @@
+define(['app','lodash','services/locale'], function(app,_) {
+  var countries, inProgress;
+  app.filter("country",['mongoStorage','locale', function(mongoStorage,locale) {
+    inProgress = mongoStorage.getCountries().then(function(data){
+      countries=data;
+      inProgress = false;
+    });
+  	return function(text) {
+
+  		if(!text)
+  			return '';
+
+          var country;
+          if(_.isObject(text))
+            country = _.find(countries,text);
+          else
+            country = _.find(countries,{identifier:text});
+
+          if(!countries) throw 'Error no countries loaded';
+          if(country)
+              return country.name[locale];
+          else{
+            console.log('no country found',text);
+            console.log('no country found',countries);
+            return 'NO COUNTRY FOUND';
+          }
+
+
+
+  	};
+  }]);
+});
