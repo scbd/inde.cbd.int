4c0d515724d59bf4c77438922680d553b9d4bfa9
Randy Houlahan <randy.houlahan@cbd.int>
2016-11-05 16:07:12 -0400

    properly remove html entities

--- app/services/filters.js
+++ app/services/filters.js
@@ -25,10 +25,29 @@ define(['app','moment','lodash'], function (app,moment,_) {
           }
           return input;
       };
-  })
+  });
+  var decodeEntities = (function() {
+  // this prevents any overhead from creating the object each time
+  var element = document.createElement('div');
+
+  function decodeHTMLEntities (str) {
+    if(str && typeof str === 'string') {
+      // strip script/html tags
+      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+      element.innerHTML = str;
+      str = element.textContent;
+      element.textContent = '';
+    }
+
+    return str;
+  }
+
+  return decodeHTMLEntities;
+})();
   app.filter('htmlToPlaintext', function() {
       return function(text) {
-        return  text ? String(text).replace(/<[^>]+>/gm, '') : '';
+        return  text ? decodeEntities(text) : '';
       };
     }
   );
